e2efa5027643f0ad01645f9e78f561a88105d8cf
Will <me@willcannings.com>
2011-10-06 16:08:01 +1100

    Hiding queue pages from admin

--- lib/migrations/01_queue_page_model.rb
+++ lib/migrations/01_queue_page_model.rb
@@ -2,6 +2,9 @@ class QueuePageModelMigration < Migration
   def self.up(site)
     site.pages.create_model :queue_pages do |queue_pages|
       queue_pages.record_class_name = 'QueuePage'
+      queue_pages.allowed_children = []
+      queue_pages.allowed_parents = []
+      queue_pages.hide_in_admin = true
     end
     
     # default queue page
